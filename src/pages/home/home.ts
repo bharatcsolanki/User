488554de5f6ce8e404088177026c7e070f9998cd
Bharat Solanki <bharat.c.solanki@hotmail.com>
2018-02-11 18:20:47 +0530

    create branch UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	form;
+	
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController) {
-
+  this.form = new FormGroup({
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
+      emailId: new FormControl("", Validators.required),
+      mobNo: new FormControl("", Validators.required),
+      city: new FormControl("", Validators.required)
+});
   }
+  processForm() {
+    let msg=this.alertCtrl.create({title:"Invalid Data",message:"All Required Values",buttons:[{text:'Ok',}]});
+    let alert = this.alertCtrl.create({
+      title: "Account Created",
+      message: "Created Account for: " + this.form.value.firstName + " " + this.form.value.lastName + " " + this.form.value.emailId + " " + this.form.value.mobNo+ " " + this.form.value.city,
+      buttons: [{
+        text: 'Ok',
+      }]
+    });
+
+    if (this.form.status === 'VALID') {
+      alert.present()
+    }
+    if (this.form.status === 'INVALID') {
+      msg.present()
+    }
+}
 
 }
